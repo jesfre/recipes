af501febb2a5c458cbafd96086c2b93ae9872715
Jorge Ruiz-Aquino <jesfre.gy@gmail.com>
2019-02-28 08:06:13 -0500

    Added InfexController. Added template with Thymeleaf. #4

---
+++ src/main/java/com/blogspot/jesfre/recipes/controllers/IndexController.java
@@ -0,0 +1,26 @@
+package com.blogspot.jesfre.recipes.controllers;
+
+import com.blogspot.jesfre.recipes.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+public class IndexController {
+
+    private RecipeService recipeService;
+
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage(Model model) {
+        log.debug("Loading index page.");
+        model.addAttribute("recipes", recipeService.getRecipes());
+        return "index";
+    }
+
+}

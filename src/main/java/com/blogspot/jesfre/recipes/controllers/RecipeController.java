3248c79b598485818f06fa4cfac7b5be50baeee9
Jorge Ruiz-Aquino <jesfre.gy@gmail.com>
2019-03-15 04:38:18 -0400

    Changes to RecipeController to add save/update mappings.

--- src/main/java/com/blogspot/jesfre/recipes/controllers/RecipeController.java
+++ src/main/java/com/blogspot/jesfre/recipes/controllers/RecipeController.java
@@ -1,12 +1,16 @@
 package com.blogspot.jesfre.recipes.controllers;
 
+import com.blogspot.jesfre.recipes.commands.RecipeCommand;
 import com.blogspot.jesfre.recipes.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 
 @Slf4j
@@ -28,6 +32,35 @@ public class RecipeController {
         return "recipe/show";
     }
 
+    @GetMapping("recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @GetMapping("recipe/{id}/update")
+    public String updateRecipe(@PathVariable String id, Model model){
+        model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
+        return RECIPE_RECIPEFORM_URL;
+    }
+
+    @PostMapping("recipe")
+    public String saveOrUpdate(@Valid @ModelAttribute("recipe") RecipeCommand command, BindingResult bindingResult){
+
+        if(bindingResult.hasErrors()){
+
+            bindingResult.getAllErrors().forEach(objectError -> {
+                log.debug(objectError.toString());
+            });
+
+            return RECIPE_RECIPEFORM_URL;
+        }
+
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/" + savedCommand.getId() + "/show";
+    }
 
     @GetMapping("recipe/{id}/delete")
     public String deleteById(@PathVariable String id){
@@ -52,5 +85,6 @@ public class RecipeController {
 
         return modelAndView;
     }
+
 
 }

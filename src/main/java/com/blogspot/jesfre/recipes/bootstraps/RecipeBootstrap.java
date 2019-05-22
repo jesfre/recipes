ba195769cd6c0869024561ef33dc2b07ff8e0aac
Jorge Ruiz Aquino <jesfre.gy@gmail.com>
2019-05-22 06:09:04 -0400

    Added bootstrap configuration for H2 only.

--- src/main/java/com/blogspot/jesfre/recipes/bootstraps/RecipeBootstrap.java
+++ src/main/java/com/blogspot/jesfre/recipes/bootstraps/RecipeBootstrap.java
@@ -6,6 +6,7 @@ import com.blogspot.jesfre.recipes.repositories.RecipeRepository;
 import com.blogspot.jesfre.recipes.repositories.UnitOfMeasureRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,6 +19,7 @@ import java.util.Optional;
 
 @Slf4j
 @Component
+@Profile("default")
 public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
     private final CategoryRepository categoryRepository;
@@ -35,7 +37,7 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
     @Transactional
     public void onApplicationEvent(ContextRefreshedEvent event) {
         recipeRepository.saveAll(getRecipes());
-        log.debug("Loading Bootstrap Data");
+        log.debug("Loading Bootstrap Data for profile=default");
     }
 
     private List<Recipe> getRecipes() {
